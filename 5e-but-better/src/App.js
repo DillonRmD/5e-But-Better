9ae7ea1f0ec4332aacab47b084338d2625127f98
Dillon Williams <dillonwilliams2001@yahoo.com>
2022-10-31 15:02:11 -0500

    Added basic player stats and player structure

--- 5e-but-better/src/App.js
+++ 5e-but-better/src/App.js
@@ -1,8 +1,56 @@
+import { Grid } from "@mui/material";
+import "./App.css"
 
-function App() {
+const App = () => {
+
+  let player = {
+    "Name": "Arthak",
+    "Lvl": 5,
+    Spells: [
+      "Acid Arrow",
+      "Moonbeam",
+    ],
+    Skills: {
+      "Strength": [14, 2],
+      "Dexterity": [13, 1],
+      "Constitution": [13, 1],
+      "Intelligence": [12, 1],
+      "Wisdom": [14, 2],
+      "Charisma": [10, 0]
+    }
+  };
+
+
+
   return (
-    <div>
-    </div>
+    <Grid container direction="row" style={{ width: "50%" }} className={"border"}>
+      <Grid container direction="row" className={"border"}>
+        <Grid item xs={6} className={"border"}>
+          <h2>Character Name: {player.Name}</h2>
+        </Grid>
+        <Grid item xs={6} className={"border"}>
+          <h2>Character Level: {player.Lvl}</h2>
+        </Grid>
+      </Grid>
+
+      <Grid container className={"border"}>
+        <Grid container className={"border"}>
+          <Grid item className={"border"}>
+            {Object.keys(player.Skills).map((key, index) => (
+              <h2>{key} with {player.Skills[key][0]} + {player.Skills[key][1]}</h2>
+            ))} 
+          </Grid>
+        </Grid>
+        <Grid container className={"border"}>
+        </Grid>
+        <Grid container className={"border"}>
+        </Grid>
+        <Grid container className={"border"}>
+        </Grid>
+      </Grid>
+
+    </Grid>
+
   );
 }
 
